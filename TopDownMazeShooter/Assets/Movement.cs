d0742cbf677742f8ac562cdeb08f589611a50787
Mathias Holst <mfho.skp@edu.mercantec.dk>
2020-06-15 13:53:13 +0200

    +Player sprite +Wall sprite +Starter map

---
+++ TopDownMazeShooter/Assets/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 4;
+
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var inputHorizontal = Input.GetAxis("Horizontal");
+        var inputVertical = Input.GetAxis("Vertical");
+
+
+        Vector2 position = transform.position;
+        position.x += speed * Time.deltaTime * inputHorizontal; //frame time independant
+        position.y += speed * Time.deltaTime * inputVertical;
+        rigidbody.MovePosition(position);
+    }
+    /*
+     * Box collider 2D til at lave mur objekter som blokere brugeren
+     * Box collider 2D til at få spilleren til ikke at kunne gå igennem vægge.
+     * Rigidbody 2D skal tilføjes til spilleren og husk at slå gravity fra da det er et topdown spil
+     * 
+     * Et gameobjekt samler på components
+     * En scene samler alle gameojbekter i den scene
+     * Et game samler på alle scener
+     * 
+     * ---MonoBehaviour Lifecycle---
+     * Start
+     * Update
+     * LateUpdate
+     * OnMouseXXX
+     * FixedUpadte
+     * https://docs.unity3d.com/Manual/ExecutionOrder.html
+     * ---Mads bruger disse til at lave spil---
+    */
+}
