38e04f13cfb9342d1b58ef8a7c1eb179ac443f7c
Mathias Holst <mfho.skp@edu.mercantec.dk>
2020-06-16 11:41:12 +0200

    +CameraScript +Enemies +hp til models +LookAtMouse

---
+++ TopDownMazeShooter/Assets/Stats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public int health = 100;
+    public int damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+             Destroy(this.gameObject);
+        }
+    }
+}
